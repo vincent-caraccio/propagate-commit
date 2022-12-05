cab8197b904a966ed11839c63140c7c0764a5c76
Vincent Caraccio <vincent.caraccio@nibbio.io>
2022-12-05 12:22:27 +0800

    new test

--- index.js
+++ index.js
@@ -21,16 +21,18 @@ const github = require('@actions/github');
 
 async function pushEmptyCommit(octokit, destOwner, destRepo, destBranch, commitMessage) {
   const latestSha = await getLatestSha(octokit, destOwner, destRepo, destBranch);
+  console.log('latestSha', latestSha);
   const newCommitSha = await createCommit(octokit, destOwner, destRepo, commitMessage, latestSha);
+  console.log('newCommitSha', newCommitSha);
   return updateRef(octokit, destOwner, destRepo, destBranch, newCommitSha);
 }
 
 async function getLatestSha(octokit, owner, repo, branch) {
   const { data } = await octokit.request(
-    'GET /repos/{owner}/{repo}/git/ref/{ref}',
-    { owner, repo, ref: `heads/${branch}` }
+    'GET /repos/{owner}/{repo}/git/commits',
+    { owner, repo, sha: branch, per_page: 1 }
   );
-  return data.object.sha;
+  return data[0].commit.tree.sha;
 }
 
 async function createCommit(octokit, owner, repo, message, tree) {
